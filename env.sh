ff63fcd4ef741843e9ded129dddc1f65c08c2cfc
nick <nick@eos>
2026-01-10 15:30:25 -0500

    feat(env): add gc alias for automated commit generation
    Introduce a new `gc` alias to automate the generation and formatting of git
    commit messages. This alias integrates a custom generation script with a
    markdown formatter to provide a pre-populated commit template, streamlining the
    workflow for maintaining conventional commit standards.

--- env.sh
+++ env.sh
@@ -1,2 +1,3 @@
 #Use RipGrep to ignore .git and venv files
 export FZF_DEFAULT_COMMAND='rg --files --hidden --follow'
+alias gc="./bin/autocommit_gen.py > /tmp/commit; ./bin/mdformat /tmp/commit | git commit -F - -e"
