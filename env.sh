ab56a16f90a121698c61896b8915f58b027cd09c
nerp <nerp@selene.tartarus.info>
2026-01-13 07:53:32 -0500

    Aliasing "Vybz Commit" to vc

--- env.sh
+++ env.sh
@@ -1,3 +1,3 @@
 #Use RipGrep to ignore .git and venv files
 export FZF_DEFAULT_COMMAND='rg --files --hidden --follow'
-alias gc="vybz-commit > /tmp/commit; vybz-fmt /tmp/commit | git commit -F - -e"
+alias vc="vybz-commit > /tmp/commit; vybz-fmt /tmp/commit | git commit -F - -e"
